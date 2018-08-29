12efa691dfad5ea26a8522ae1f9710498255e93f
arnoldpalmer42 <40504988+arnoldpalmer42@users.noreply.github.com>
2018-08-28 17:09:54 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Sept 5, 2018 09:30:00").getTime();
+var countDownDate = new Date("Sept 1, 2018 09:30:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
